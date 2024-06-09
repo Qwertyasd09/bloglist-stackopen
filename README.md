f0166c1dbc65449595d3c3a4f8729301d07d8a4c
Diego Armando Chiri Torres <dgchiri@gmail.com>
2024-06-09 01:46:14 -0400

    exercises 4.8-4.12 done

--- README.md
+++
@@ -1,4 +0,0 @@
-Exercises 4.1.-4.2.
-Note: this course material was written with version v20.11.0 of Node.js. Please make sure that your version of Node is at least as new as the version used in the material (you can check the version by running node -v in the command line).
-
-In the exercises for this part, we will be building a blog list application, that allows users to save information about interesting blogs they have stumbled across on the internet. For each listed blog we will save the author, title, URL, and amount of upvotes from users of the application.
--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,57 +1,19 @@
-const { test, describe } = require('node:test')
+const { test, describe, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require("../models/blog")
 
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }  
-]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('dummy returns one', () => {
   const blogs = []
@@ -79,14 +41,94 @@ describe('total likes', () => {
 })
 
 test('blog with most likes', () => {
-
   const blogWithMostLikes = {
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     likes: 12
   }
-
-  const result = listHelper.favoriteBlog(blogs)
-
+  const result = listHelper.favoriteBlog(helper.initialBlogs)
   assert.deepStrictEqual(result, blogWithMostLikes)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blog with most likes', () => {
+  const blogWithMostLikes = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12
+  }
+  const result = listHelper.favoriteBlog(helper.initialBlogs)
+  assert.deepStrictEqual(result, blogWithMostLikes)
+})
+
+test('blog with id property instead of _id', async () => {
+  const result = await helper.blogsInDb()
+  assert(Object.keys(result[0]).includes("id") && !Object.keys(result[0]).includes("_id"))
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "cRINGE",
+    author: "DEFEG",
+    url: "http://DS.htmll",
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  assert(titles.includes('cRINGE'))
+})
+
+test('a blog without likes can be added ', async () => {
+  const newBlog = {
+    title: "jajajaja",
+    author: "ola k ase",
+    url: "http://www.mll"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  assert.strictEqual(blogsAtEnd.find(blog => blog.title === "jajajaja").likes, 0)
+})
+
+test.only('blog without title or url is not added', async () => {
+  const newBlog = {
+    author: "DEFEG",
+    url: "http://DS.htmll",
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
 })
